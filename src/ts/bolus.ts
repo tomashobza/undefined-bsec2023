4de30f3875f2d0adbeb2c8d898cfb23d1013eb05
Tomáš Hobza <thomashobza@ids.company>
2023-02-23 23:47:18 +0100

    check data validity

--- src/ts/bolus.ts
+++ src/ts/bolus.ts
@@ -1,6 +1,7 @@
 import type { Food, HistoryRecord } from './data';
 
 export const calculateBolus = (food: Food) => {
+  // least square calculations
   const x: number[] = [];
   const y: number[] = [];
   const x2: number[] = [];
@@ -8,6 +9,8 @@ export const calculateBolus = (food: Food) => {
   const jvbs: number[] = [];
 
   food.arr.forEach((foodRecord: HistoryRecord) => {
+    if (!foodRecord?.Result || !foodRecord?.Initial || !foodRecord?.Bolus) return; // food record not complete
+
     x.push(foodRecord.Result-foodRecord.Initial);
     x2.push((foodRecord.Result-foodRecord.Initial)*(foodRecord.Result-foodRecord.Initial));
     y.push(foodRecord.Bolus);
@@ -21,9 +24,11 @@ export const calculateBolus = (food: Food) => {
   const sumY = y.reduce((a, v) => a+v);
   const avgJVB = jvbs.reduce((a, v) => a+v) / jvbs.length;
 
+  // calculate y=mx+b for bolus
   const b = (x.length*sumXY - sumX*sumY)/(x.length*sumX2 - sumX*sumX) || 0;
   const a = (sumY - b*sumX)/x.length;
 
+  // write calculated data to data structure
   (food as any)['a'] = a;
   (food as any)['b'] = b;
   (food as any)['avgJVB'] = avgJVB;
