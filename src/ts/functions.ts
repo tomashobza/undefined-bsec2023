d1cd83075dd9f5b175d9b650e38a97fd5ea95b04
jakubblaha-ids <jakubblaha@ids.company>
2023-02-24 00:27:54 +0100

    new bolus

--- src/ts/functions.ts
+++ src/ts/functions.ts
@@ -76,3 +76,59 @@ export function updateMealRecord(recordId: string, result: number): MealRecord {
 
   return record;
 }
+
+export function getMealRecordsOfType(typeId: string): MealRecord[] {
+  const allRecords = getMealRecords();
+
+  const ofType = allRecords.filter((r) => r.mealTypeId === typeId)
+
+  return ofType;
+}
+
+export function calculateBolus(mealTypeId: string): {a: number, b: number, avgJVB: number} {
+  const records = getMealRecordsOfType(mealTypeId);
+
+  // least square calculations
+  const x: number[] = [];
+  const y: number[] = [];
+  const x2: number[] = [];
+  const xy: number[] = [];
+  const jvbs: number[] = [];
+
+  records.forEach((foodRecord: MealRecord) => {
+    if (!foodRecord.result) {
+      return
+    }
+
+    x.push(foodRecord.result - foodRecord.init);
+    x2.push((foodRecord.result - foodRecord.init) * (foodRecord.result - foodRecord.init));
+    y.push(foodRecord.dose);
+    xy.push((foodRecord.result - foodRecord.init) * foodRecord.dose);
+    jvbs.push(foodRecord.dose + (foodRecord.result - foodRecord.init));
+  });
+
+
+  records.forEach((foodRecord: MealRecord) => {
+    if (!foodRecord.result) {
+      return
+    }
+
+    x.push(foodRecord.result - foodRecord.init);
+    x2.push((foodRecord.result - foodRecord.init) * (foodRecord.result - foodRecord.init));
+    y.push(foodRecord.dose);
+    xy.push((foodRecord.result - foodRecord.init) * foodRecord.dose);
+    jvbs.push(foodRecord.dose + (foodRecord.result - foodRecord.init));
+  });
+
+  const sumXY = xy.reduce((a, v) => a+v);
+  const sumX = x.reduce((a, v) => a+v);
+  const sumX2 = x2.reduce((a, v) => a+v);
+  const sumY = y.reduce((a, v) => a+v);
+  const avgJVB = jvbs.reduce((a, v) => a+v) / jvbs.length;
+
+  // calculate y=mx+b for bolus
+  const b = (x.length*sumXY - sumX*sumY)/(x.length*sumX2 - sumX*sumX) || 0;
+  const a = (sumY - b*sumX)/x.length;
+
+  return {a, b, avgJVB};
+}
