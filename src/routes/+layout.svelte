f3f877303f7cc05f729ead4862428732389ac0f3
KociTomas03 <koci.tomas03@gmail.com>
2023-02-24 02:43:10 +0100

    idk

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 	// import '$ts/auth'
 	// import '$ts/data';
 	// import '$ts/restaurants'
-	
+
 </script>
 
 <svelte:head>
@@ -17,11 +17,11 @@
 
 <div class="sm:p-4 transition-all bg-slate-100 w-full h-full flex flex-col items-center justify-center text-sm">
 	<div class="mobile">
-		<div class="w-full grid grid-cols-3 place-items-center p-2 bg-[#35B9F1] h-10">
-				<button class="w-6"><Home></Home></button>
+		<div class="w-full grid grid-cols-3 place-items-center pt-2 bg-[#35B9F1] h-10">
+			<button class="w-6"><Home></Home></button>
 			<button class="w-4"><Food></Food></button>
 			<button class="w-6"><Graph></Graph></button>
-			<!-- TODO: tady tome -->
+			<div class="w-1/3 bg-white rounded-lg h-1 justify-end mt-[0.33rem]"></div>
 		</div>
 
 		<div class="p-4 h-full overflow-y-auto flex-grow">
