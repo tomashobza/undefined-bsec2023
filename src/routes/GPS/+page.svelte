1763a5e27600f98fbfd2b3101e1ec7e4e88f65f2
KociTomas03 <koci.tomas03@gmail.com>
2023-02-24 01:15:04 +0100

    GPS - get location

---
+++ src/routes/GPS/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  // import { history } from "$ts/data";
+const successCallback = (position) => {
+  console.log(position);
+};
+
+const errorCallback = (error) => {
+  console.log(error);
+};
+function GPS(){
+	navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+}
+</script>
+
+<svelte:head>
+	<title></title>
+	<meta name="description" content="" />
+</svelte:head>
+
+<div class="w-full h-screen flex items-center justify-center"><button class="p-3 rounded-lg bg-stone-400" on:click={GPS}>GPS(v konzoli)</button></div>
+
+<style lang="postcss">	
+</style>
