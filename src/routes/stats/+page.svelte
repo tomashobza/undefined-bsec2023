df11eaad6a93ac095fa027bc372630128086db79
Tomáš Hobza <thomashobza@ids.company>
2023-02-24 06:46:29 +0100

    add: graf

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -5,10 +5,11 @@
     import { onMount } from "svelte"
     import dayjs from "dayjs"
     import Chart from 'chart.js/auto';
+  import Chevron from "$icons/Chevron.svelte";
 
     let records: MealRecord[] = [];
     let groups: {dateString: string, records: MealRecord[]}[] = [];
-
+    let cnvs: HTMLCanvasElement;
     onMount(() => {
       records = getSortedMealRecordsByDate();
 
@@ -30,11 +31,47 @@
       })
 
       groups = groups
+
+      console.log(records);
+
+
+      new Chart(cnvs, {
+      type: 'bar',
+      data: {
+        labels: records.map(v => dayjs(v.dateTime*1000).format('DD.MM.')),
+        datasets: [
+          {
+            label: "glykemie",
+            backgroundColor: ["#F5EAEA", "#FFB84C","#F16767"],
+            data: records.map(v => v.init)
+          }
+        ]
+      },
+      options: {
+        indexAxis: 'y',
+        legend: { display: false },
+        plugins: {
+        legend: {
+            display: false,
+         } }
+      }
+  });
     })
+
+
+    let showGraph = false;
+    const toggleGraph = () => showGraph = !showGraph;
+  
 </script>
 
 <div class="pb-3">
-  <div>graf</div>
+  <button class="w-full flex flex-row items-center mb-2" on:click={toggleGraph}>
+    <div class="flex-grow text-left">Ukázat graf posledních 30 dní</div>
+    <div class="w-5 transition-all duration-500" class:rotate-180={showGraph}><Chevron /></div>
+  </button>
+  <div class="w-full overflow-y-scroll" style="height: {showGraph ? 'auto' : '0'}">
+    <canvas bind:this={cnvs} class="w-full" height="2000"></canvas>
+  </div>
   {#each groups as group}
     <div class="py-2 px-2 mt-4 font-semibold">{group.dateString}</div>
   
